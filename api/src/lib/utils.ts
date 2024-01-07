cb35b3b7ef8c4a564970b481dbbf2849afd4e7a3
Zach Lankton <zachlankton@gmail.com>
2024-01-07 15:01:09 -0500

    create base62 ID generator

--- api/src/lib/utils.ts
+++ api/src/lib/utils.ts
@@ -1,7 +1,67 @@
 import crypto from "crypto";
 
-export function shortID(length: number) {
-  return crypto.randomUUID().replaceAll("-", "").slice(0, length);
+const BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const bigInt62 = BigInt(62);
+
+export function generateID() {
+  // Generate a 128-bit number (16 bytes)
+  const buffer = crypto.randomBytes(16);
+
+  // Convert the buffer to a BigInt
+  let number = BigInt("0x" + buffer.toString("hex"));
+
+  // Convert the BigInt to a Base62 string
+  let base62 = "";
+  while (number > 0) {
+    base62 = BASE62[Number(number % bigInt62)] + base62;
+    number = number / bigInt62;
+  }
+
+  // Pad the string to ensure a length of 22 characters
+  base62 = base62.padStart(22, "0");
+
+  return base62;
+}
+
+export function uuidToBase62(uuid: string) {
+  // Remove hyphens from UUID
+  let noHyphensUUID = uuid.replace(/-/g, "");
+
+  // Convert UUID (hexadecimal) to BigInt
+  let num = BigInt("0x" + noHyphensUUID);
+
+  // Convert the BigInt to a Base62 string
+  let base62 = "";
+  while (num > 0) {
+    base62 = BASE62[Number(num % bigInt62)] + base62;
+    num = num / bigInt62;
+  }
+  return base62;
+}
+
+export function base62ToUUID(base62: string) {
+  let num = BigInt(0);
+
+  // Convert from Base62 to BigInt
+  for (let char of base62) {
+    num = num * BigInt(62) + BigInt(BASE62.indexOf(char));
+  }
+
+  // Convert BigInt to a hexadecimal string
+  let hex = num.toString(16).padStart(32, "0");
+
+  // Format the hexadecimal string as a UUID
+  return (
+    hex.substring(0, 8) +
+    "-" +
+    hex.substring(8, 12) +
+    "-" +
+    hex.substring(12, 16) +
+    "-" +
+    hex.substring(16, 20) +
+    "-" +
+    hex.substring(20)
+  );
 }
 
 export async function dataOrError(resp: Response) {
